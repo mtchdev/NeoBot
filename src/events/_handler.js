b313ae847cbeb0a979404309296260f6ea09d895
whenofficial <fsmitchm@gmail.com>
2019-03-14 05:12:54 -0700

    Add guild delete event

--- src/events/_handler.js
+++ src/events/_handler.js
@@ -1,5 +1,6 @@
 const event = (event) => require(`./${event}`);
 module.exports = client => {
     client.on('guildCreate', event('add'));
+    client.on('guildDelete', event('remove'));
     client.on('message', event('message'));
 };
