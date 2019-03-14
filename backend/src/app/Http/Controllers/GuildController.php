f8ad9513cbebb316a0f49be6b5e90f5f9fa5f990
whenofficial <fsmitchm@gmail.com>
2019-03-14 04:54:31 -0700

    fix

--- backend/src/app/Http/Controllers/GuildController.php
+++ backend/src/app/Http/Controllers/GuildController.php
@@ -7,12 +7,12 @@ use App\Guild;
 
 class GuildController extends Controller
 {
-    add(Request $request) {
+    function add(Request $request) {
         $guild = new Guild;
 
         $guild->guild_id = $request->input('guild_id');
         $guild->owner_id = $request->input('owner_id');
 
-        $guild-save();
+        $guild->save();
     }
 }
