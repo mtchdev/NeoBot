27e81e830bc3909fb4c3be641611c693d833ba5d
whenofficial <fsmitchm@gmail.com>
2019-03-14 04:28:10 -0700

    Add guild join function

--- backend/src/app/Http/Controllers/GuildController.php
+++ backend/src/app/Http/Controllers/GuildController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Guild;
 
 class GuildController extends Controller
 {
     add(Request $request) {
-        
+        $guild = new Guild;
+
+        $guild->guild_id = $request->input('guild_id');
+        $guild->owner_id = $request->input('owner_id');
+
+        $guild-save();
     }
 }
