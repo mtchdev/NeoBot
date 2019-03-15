f9c55fc29ba17d39c9f29be256cd797e1f25275f
whenofficial <fsmitchm@gmail.com>
2019-03-15 03:42:19 -0700

    Add warning command

--- src/plugins/warn/warn.js
+++ src/plugins/warn/warn.js
@@ -1,3 +1,24 @@
+const meta = require('./warn.json');
+const wMessage = require('../../messages/warning');
+const sMessage = require('../../messages/success');
+
 exports.run = (message, client, args) => {
-    
+    // !warn [user] [reason]
+
+    if(args[0] === null) return;
+    if(args[1] === null) return;
+
+    let reason = args.slice(1).join(' ');
+    let user = message.mentions.users.first();
+
+    if(typeof user === 'undefined') {
+        message.delete();
+        wMessage('Please @mention a user to warn.', message);
+        return;
+    }
+
+    user.send(`You were warned on ${message.guild.name}: ${reason}`);
+
+    sMessage(`Warned ${user} for ${reason}`, message);
+
 }
