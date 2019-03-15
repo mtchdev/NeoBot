cbfd50c69c52ad440653db6a8ac32bf48a379286
whenofficial <fsmitchm@gmail.com>
2019-03-15 05:55:58 -0700

    Add new case model

---
+++ backend/src/database/migrations/2019_03_15_125407_add_punishments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPunishmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cases', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('case', 255);
+            $table->string('type');
+            $table->string('user', 255);
+            $table->string('reason', 255);
+            $table->string('actor', 255);
+            $table->string('expiry', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
