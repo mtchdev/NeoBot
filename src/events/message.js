6a9c52f0d0eeedf315d52e1dc1f715bfa7cf0403
whenofficial <fsmitchm@gmail.com>
2019-03-15 03:23:27 -0700

    Fix directory

--- src/events/message.js
+++ src/events/message.js
@@ -29,7 +29,7 @@ module.exports = message => {
 
         // Run command
 
-        let file = require(`./plugins/${cmd}/${cmd}.js`);
+        let file = require(`../plugins/${cmd}/${cmd}.js`);
         file.run(message, client, args);
         
     })
