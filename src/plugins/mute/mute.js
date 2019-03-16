ad90756257be9fc4b18f2fca47ddca08cafa5be2
whenofficial <fsmitchm@gmail.com>
2019-03-16 08:36:05 -0700

    Check if role exists

--- src/plugins/mute/mute.js
+++ src/plugins/mute/mute.js
@@ -5,8 +5,6 @@ const axios = require('axios');
 
 exports.run = (message, client, args) => {
 
-    console.log(message.guild.roles);
-
     if(['-init', '-i'].indexOf(args[0])+1){
         // RUN SETUP OF MUTE FEATURE
         message.guild.createRole({
@@ -33,6 +31,8 @@ exports.run = (message, client, args) => {
 
     axios.get('http://localhost:8000/config/roles/muted/get', {headers:{guild_id:message.guild.id}}).then(res => {
         if(['', null].indexOf(res.data.role) +1)
+            return wMessage('The mute feature isn\'t set up yet! Run `!mute -init` to begin.', message);
+        if(!message.guild.roles.has(res.data.role))
             return wMessage('The mute feature isn\'t set up yet! Run `!mute -init` to begin.', message);
 
         if(!args[0])
