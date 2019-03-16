0300e7217552a2484cadc49015c9797c75bc90dc
whenofficial <fsmitchm@gmail.com>
2019-03-16 07:12:34 -0700

    add base mute command

---
+++ src/plugins/mute/mute.js
@@ -0,0 +1,10 @@
+const meta = require('./case.json');
+const wMessage = require('../../messages/warning');
+const sMessage = require('../../messages/success');
+const axios = require('axios');
+
+exports.run = (message, client, args) => {
+
+    
+
+}
