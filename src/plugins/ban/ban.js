fa980e7b0486c4756fb5c0f4b41e3001ae4e18c6
whenofficial <fsmitchm@gmail.com>
2019-03-16 04:07:43 -0700

    Add backend link

--- src/plugins/ban/ban.js
+++ src/plugins/ban/ban.js
@@ -9,7 +9,7 @@ exports.run = (message, client, args) => {
     if(!args[1]) return;
 
     let reason = args.slice(1).join(' ');
-    let user = message.mentions.users.first();
+    let user = message.mentions.members.first();
 
     if(!user)
         return wMessage('Please @mention a valid user.', message);
@@ -18,10 +18,24 @@ exports.run = (message, client, args) => {
         return wMessage('That user cannot be banned.', message);
 
     user.send('You were banned on '+message.guild.name+' for '+reason).then(() => {
-        user.ban(reason).catch(err => {
+        user.ban(reason)
+        .then(() => {
+            let data = {
+                guild_id: message.guild.id,
+                user: user.id,
+                reason: reason,
+                actor: message.author.id
+            }
+            axios.post('http://localhost:8000/ban/new', data).then(res => {
+                if(res.data.message !== 200) return;
+                sMessage('`[CASE #'+res.data.case+']` Banned '+user+' for '+reason, message);
+            }).catch(err => {
+                wMessage(err, message);
+            });
+        })
+        .catch(err => {
             wMessage(err, message);
         });
-        sMessage(user+' was banned for '+reason);
     });
 
 }
