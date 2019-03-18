f770797ff4f9cae1da0f2eeef4eb1a5364fa36e8
whenofficial <fsmitchm@gmail.com>
2019-03-18 04:25:17 -0700

    Check if user is muted on join, then add role

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,5 +1,16 @@
+const axios = require('axios');
+
 module.exports = (member, client, message, guild) => {
-    
+
     let userid = member.user.id;
+    axios.get('http://localhost:8000/mute/get', {headers:{user_id:userid}}).then(res => {
+        if(res.data.message !== 200) return;
+        if(res.data.data == null) return;
+        axios.get('http://localhost:8000/config/roles/muted/get', {headers:{guild_id:member.guild.id}}).then(resp => {
+            if(resp.data.role !== null) {
+                member.addRole(resp.data.role)
+            }
+        });
+    });
 
 }
