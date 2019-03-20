5fcfa650e867848e0046bb334a4e3e1c99d3b66d
whenofficial <fsmitchm@gmail.com>
2019-03-20 15:19:40 +0800

    Cleanup

--- src/commands/unmute/unmute.js
+++ src/commands/unmute/unmute.js
@@ -18,15 +18,18 @@ class Unmute extends Command {
     
         let user = message.mentions.members.first();
 
-        axios.get('http://localhost:8000/config/roles/muted/get', {headers:{guild_id:message.guild.id}}).then(res => {
+        try {
+            let res = await Command.prototype.apiget('http://localhost:8000/config/roles/muted/get', {headers:{guild_id:message.guild.id}});
             if(!user.roles.has(res.data.role))
                 return Command.prototype.warn('User is not muted!', message);
             if(user.id == message.author.id)
                 return Command.prototype.warn('You cannot unmute yourself.', message);
             await this.unmute(message.guild.id, user.id, message.author.id, res);
-
-        });
-    }
+        } catch (err) {
+            Command.prototype.warn(err, message);
+            return;
+        }
+    } 
 
     async unmute(guild, user, actor, res) {
         let data = {
@@ -34,15 +37,16 @@ class Unmute extends Command {
             user: user,
             actor: actor
         }
-        await Command.prototype.apipost('http://localhost:8000/mute/unmute', data).then(resp => {
+
+        try {
+            let resp = await Command.prototype.apipost('http://localhost:8000/mute/unmute', data);
             user.removeRole(res.data.role);
             Command.prototype.success('`[CASE #'+resp.data.case+']` Unmuted **'+user.user.username+'#'+user.user.discriminator+'**.', message);
             user.send(`You were unmuted on ${message.guild.name}.`);
-            return false;
-        }).catch(err => {
+        } catch (err) {
             Command.prototype.warn(err, message);
-            return false;
-        });
+            return;
+        }
 
         return false;
     }
