37ee835afc0d52ee33efffa7c0342369b916c4c6
whenofficial <fsmitchm@gmail.com>
2019-03-21 15:51:45 +0800

    Add logger events

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json');
+const Logger = require('./handlers/logger');
+
 require('./events/_handler')(client);
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  new Logger().log('Bot online as '+client.user.tag, 4);
 });
 
 client.login(settings.token);
