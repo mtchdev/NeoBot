a1ef06fa0705cf32c457c302c996b8401cc31c66
whenofficial <fsmitchm@gmail.com>
2019-03-25 11:50:56 +0800

    fix

--- src/commands/addrole/Addrole.js
+++ src/commands/addrole/Addrole.js
@@ -5,26 +5,26 @@ class AddRole extends Command {
     constructor(){
         super({
             name: 'Add role',
-            info: 'Add a role to a user',
+            info: 'Add a role to a user. The role name is CaSe seNSitIvE',
             usage: 'addRole [user] [role]'
         });
     }
 
     async execute(message, client, args) {
+        if(['help', 'h'].indexOf(args[0])+1)
+        return super.cmdhelp(message);
+        
         let user = message.mentions.members.first();
 
         var role = await message.guild.roles.find(roles => roles.name == args[1]);
         if(role == null)
             return super.warn('Role not found.', message);
 
-        if(['help', 'h'].indexOf(args[0])+1)
-            return super.cmdhelp(message);
-
         if(typeof user == 'undefined')
             return super.warn('Please @mention a user on this server to add the role to.', message);
 
         if(user.roles.has(role.id))
-            return super.warn('user has role LOL', message);
+            return super.warn('User already has the **'+role.name+'** role.', message);
 
         await this.addrole(message, role, user);
     }
